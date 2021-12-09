8130955101658c4153399d28ccd45b62d2f211da
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2021-12-09 15:14:30 +0530

    Add files via upload

---
+++ doc26222222series.py
@@ -0,0 +1,9 @@
+num=int(input("Enter number terms :"))
+s=0
+sum1=0
+# print("series are ",end=" ")
+for i in range(0,num):
+    s=(s*10+2)
+    print(s,end=" ")
+    # sum1=sum1+s
+#print("series are ",end=" ")
