eba81ef41b79748b4dfb070cc35148d92659f171
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2021-12-11 14:54:06 +0530

    Add files via upload

---
+++ uand v.py
@@ -0,0 +1,35 @@
+i=1
+while i<=5:
+    j=1
+    while j<=5:
+        print(j,end="")
+        j=j+1
+    print()
+    i=i+1
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print(i,end="")
+#         j=j+1
+#     print()
+#     i=i+1
+
+# i=1
+# while i<=5:
+#     j=5
+#     while j>=1:
+#         print(i,end="")
+#         j=j-1
+#     print()
+#     i=i+1
+
+# i=1
+# while i<=5:
+#     j=5
+#     while j>=1:
+#         print(j,end="")
+#         j=j-1
+#     print()
+#     i=i+1
