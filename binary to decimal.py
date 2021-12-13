ce7c611b9abd13ee2f3965a2fbb518146c756441
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2021-12-13 15:03:26 +0530

    Add files via upload

---
+++ binary to decimal.py
@@ -0,0 +1,21 @@
+# num=int(input("Enter the number :"))
+# i=1
+# sum=0
+# while num>0:
+#     rem=num%10
+#     sum=sum+(rem*i)
+#     i=i*2
+#     num=num//10
+# print(sum)
+
+# num=int(input("Enter the number :"))
+# i=1
+# rem=0
+# while num>0:
+#     rem=(num%2)*i
+#     num=num//2
+#     i=i*10
+#     i.reverse()
+# print(i)
+
+
