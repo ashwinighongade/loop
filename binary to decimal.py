04ac31a88925e43af262860ac19717ccda688fcf
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2021-12-14 14:07:28 +0530

    Add files via upload

--- binary to decimal.py
+++ binary to decimal.py
@@ -1,12 +1,12 @@
-# num=int(input("Enter the number :"))
-# i=1
-# sum=0
-# while num>0:
-#     rem=num%10
-#     sum=sum+(rem*i)
-#     i=i*2
-#     num=num//10
-# print(sum)
+num=int(input("Enter the number :"))
+i=1
+sum=0
+while num>0:
+    rem=num%10
+    sum=sum+(rem*i)
+    i=i*2
+    num=num//10
+print(sum)
 
 # num=int(input("Enter the number :"))
 # i=1
