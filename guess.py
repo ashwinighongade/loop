98f34552879cff9219c4700452a1a2649ded1d10
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2021-12-29 20:16:17 +0530

    Delete guess.py

--- guess.py
+++
@@ -1,9 +0,0 @@
-
-i=1
-while i<=3:
-    num=int(input("Enter the number between 1 to 9 :"))
-    if i==2:
-        print("congras! your correct guess")
-    else:
-        print("worng guess")
-    i=i+1
