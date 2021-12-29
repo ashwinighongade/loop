6b6f6e145b41cafd460dc9a26c61854972496207
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2021-12-29 20:09:33 +0530

    Delete doc26222222series.py

--- doc26222222series.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter number terms :"))
-s=0
-sum1=0
-# print("series are ",end=" ")
-for i in range(0,num):
-    s=(s*10+2)
-    print(s,end=" ")
-    # sum1=sum1+s
-#print("series are ",end=" ")
