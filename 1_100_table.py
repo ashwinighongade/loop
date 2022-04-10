039a8f29844cb3ef0e4ac4aee80c831c90c67cfd
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2022-04-10 13:24:30 +0530

    Add files via upload

---
+++ 1_100_table.py
@@ -0,0 +1,23 @@
+# for i in range(1,11):
+#     while i<=100:
+#         print(i," ",end="")
+#         i+=10
+#     print()
+
+# for i in range(1,11):
+#     print(i,end="\t")
+# print()
+# print("______________________________________________________________________________\n")
+
+# for j in range(1,11):
+#     for k in range(1,11):
+#         print(j*k,end=" \t")
+#     print("\n")
+
+
+for i in range(1,11):
+    while i<=100:
+        print(i," ",end=" ")
+        i+=10
+    print()
+    print()
